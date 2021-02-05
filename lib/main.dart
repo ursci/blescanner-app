bfd2dc85ee81cb75b9ea485ab5ab9ba6499aba09
Mario Basa <mario.basa@gmail.com>
2021-02-05 13:16:55 +0900

    added permission_handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+import 'dart:io';
+
 import 'package:blescanner_app/pages/BlePage.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(BleApp());
 
 class BleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    if (Platform.isAndroid) {
+      PermissionHandler().requestPermissions(
+          [PermissionGroup.location, PermissionGroup.sensors]);
+    }
+
     return MaterialApp(
       title: "Bluetooth Device Scanner",
       theme: ThemeData(primarySwatch: Colors.indigo),
